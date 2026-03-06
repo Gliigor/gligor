aa959fc8d66f9417b6d9c847a6d9b590e033dd77
Gliigor <99903504+Gliigor@users.noreply.github.com>
2026-03-06 15:09:38 +0100

    Add BudgetMaker route to App component

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import CsvDashboard from "./pages/CsvDashboard";
+import BudgetMaker from "./pages/BudgetMaker";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -18,6 +19,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/csv-dashboard" element={<CsvDashboard />} />
+          <Route path="/budget-maker" element={<BudgetMaker />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </HashRouter>
